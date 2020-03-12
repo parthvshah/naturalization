c06799716297fb07d31a7162dcde55d61fc4518e
richa13sharma <richa13sharmaa@gmail.com>
2020-03-12 19:58:08 +0530

    tokens added

--- create.py
+++ create.py
@@ -1,15 +1,33 @@
 import re
 
-f = open("data.txt", "r")
+f = open("./data/data.txt", "r")
 lines = f.readlines()
 
-annFD = open("annotated.txt", "w")
-obFD = open("obscured.txt", "w")
+annFD = open("./data/annotated.txt", "w")
+obFD = open("./data/obscured.txt", "w")
 
 def ann(sent):
+    repSent = sent.replace(".", " ") \
+                    .replace("..", "(pause)") \
+                    .replace("...", "(pause)") \
+                    .replace("--", "(pause)") \
+                    .replace("---", "(pause)") 
+
+    repSent1 = re.sub(r"^uh+", "(uh)", repSent) 
+    repSent2 = re.sub(r"^um+", "(um)", repSent1)
+    repSent3 = re.sub(r"\suh+", " (uh)", repSent2) 
+    repSent4 = re.sub(r"\sum+", " (um)", repSent3)
+    # TODO: multiple spaces
+    repSent5 = re.sub(r" - ", "(pause)", repSent4)  
+    repSent6 = re.sub(r"-uh+", "(uh)", repSent5) 
+    repSent7 = re.sub(r"-um+", "(um)", repSent6)              
 
 
-    annFD.write(sent)
+    repSentList = list(repSent7.split())
+    repSentWOSpaces = ' '.join(repSentList)
+
+    annFD.write(repSentWOSpaces+'\n')
+    #annFD.write(sent)
 
 def ob(sent):
     repSent = sent.replace(".", " ") \
