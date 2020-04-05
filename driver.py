1a1943c28c26e88a80e29439b9699e381b8c3478
Parth Shah <parthvshah@gmail.com>
2020-04-05 11:38:10 +0530

    Results for test1

--- driver.py
+++ driver.py
@@ -1,5 +1,10 @@
 from bigram import bigramDriver
 
+def sanitize(line):
+    return line.replace("(pause)", "") \
+                .replace("(um)", "") \
+                .replace("(uh)", "")
+
 inFile = 'in.txt'
 
 IF = open(inFile, 'r')
@@ -7,5 +12,9 @@ IF = open(inFile, 'r')
 inputLines = IF.readlines()
 
 for line in inputLines:
-    print(bigramDriver(line))
+    if(len(line)<=2):
+        print()
+        continue
+    sanitizedLine = sanitize(line)
+    print(bigramDriver(sanitizedLine))
 
