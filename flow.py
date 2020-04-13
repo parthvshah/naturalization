ed73764fd8193b9873279068d1e60145e9c48b23
Parth Shah <parthvshah@gmail.com>
2020-04-13 09:21:05 +0530

    LSTM

--- flow.py
+++ flow.py
@@ -2,77 +2,76 @@ from numpy import array
 from keras.preprocessing.text import Tokenizer
 from keras.utils import to_categorical
 from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
+from keras.models import Sequential, load_model
+from keras.callbacks import ModelCheckpoint
 from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import LSTM, GRU
 from keras.layers import Embedding
  
-# generate a sequence from a language model
 def generate_seq(model, tokenizer, max_length, seed_text, n_words):
 	in_text = seed_text
-	# generate a fixed number of words
 	for _ in range(n_words):
-		# encode the text as integer
 		encoded = tokenizer.texts_to_sequences([in_text])[0]
-		# pre-pad sequences to a fixed length
 		encoded = pad_sequences([encoded], maxlen=max_length, padding='pre')
-		# predict probabilities for each word
 		yhat = model.predict_classes(encoded, verbose=0)
-		# map predicted word index to word
 		out_word = ''
 		for word, index in tokenizer.word_index.items():
 			if index == yhat:
 				out_word = word
 				break
-		# append to input
 		in_text += ' ' + out_word
 	return in_text
  
-# source text
-data = """ Jack and Jill went up the hill\n
-		To fetch a pail of water\n
-		Jack fell down and broke his crown\n
-		And Jill came tumbling after\n """
+def load_data(file_name, size):
+    data = ''
+    with open(file_name) as f:
+        head = [next(f) for x in range(size)]
+    data = ''.join(head)
+    return data
 
-# integer encode sequences of words
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts([data])
-encoded = tokenizer.texts_to_sequences([data])[0]
+def model_tol(max_length, vocab_size, X, y, load=True):
+    if(load):
+        
+        model = load_model('./data/model_LSTM.h5')
+        print("Loaded model from disk.")
+        return model
+    else:
+        model = Sequential()
+        model.add(Embedding(vocab_size, 10, input_length=max_length-1))
+        model.add(LSTM(100))
+        model.add(Dense(vocab_size, activation='softmax'))
 
-# retrieve vocabulary size
-vocab_size = len(tokenizer.word_index) + 1
-print('Vocabulary Size: %d' % vocab_size)
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        model.fit(X, y, epochs=50) # Epochs: 500
 
-# encode 2 words -> 1 word
-sequences = list()
-for i in range(2, len(encoded)):
-	sequence = encoded[i-2:i+1]
-	sequences.append(sequence)
-print('Total Sequences: %d' % len(sequences))
+        model.save('./data/model_LSTM.h5')
+        return model
 
-# pad sequences
-max_length = max([len(seq) for seq in sequences])
-sequences = pad_sequences(sequences, maxlen=max_length, padding='pre')
-print('Max Sequence Length: %d' % max_length)
+def load_tokenizer():
+    tokenizer = Tokenizer()
+    data = load_data('./data/annotated.txt', 1500) # Max: 10000
+    tokenizer.fit_on_texts([data])
+    encoded = tokenizer.texts_to_sequences([data])[0]
 
-# split into input and output elements
-sequences = array(sequences)
-X, y = sequences[:,:-1],sequences[:,-1]
-y = to_categorical(y, num_classes=vocab_size)
+    vocab_size = len(tokenizer.word_index) + 1
+    print('Vocabulary Size: %d' % vocab_size)
 
-# define model
-model = Sequential()
-model.add(Embedding(vocab_size, 10, input_length=max_length-1))
-model.add(LSTM(50))
-model.add(Dense(vocab_size, activation='softmax'))
-# print(model.summary())
+    sequences = list()
+    for i in range(2, len(encoded)):
+        sequence = encoded[i-2:i+1]
+        sequences.append(sequence)
+    print('Total Sequences: %d' % len(sequences))
 
-# compile network
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X, y, epochs=500)
+    max_length = max([len(seq) for seq in sequences])
+    sequences = pad_sequences(sequences, maxlen=max_length, padding='pre')
+    print('Max Sequence Length: %d' % max_length)
 
-# evaluate model
-print(generate_seq(model, tokenizer, max_length-1, 'Jack and', 5))
-print(generate_seq(model, tokenizer, max_length-1, 'And Jill', 3))
-print(generate_seq(model, tokenizer, max_length-1, 'fell down', 5))
-print(generate_seq(model, tokenizer, max_length-1, 'pail of', 5))
+    sequences = array(sequences)
+    X, y = sequences[:,:-1],sequences[:,-1]
+    y = to_categorical(y, num_classes=vocab_size)
+
+    return tokenizer, max_length, vocab_size, X, y
+
+tokenizer, max_length, vocab_size, X, y = load_tokenizer()
+model = model_tol(max_length, vocab_size, X, y, load=True)
+print(generate_seq(model, tokenizer, max_length-1, 'no i', 1))
